f72df9afb7c259896463236690604ad93ef273bc
GiacWebDev <giacobechini@gmail.com>
2023-11-20 19:01:43 +0100

    [add] file home.blade [remove] welcome.blade

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores itaque debitis illum repellendus, quaerat
+        quibusdam dolores unde, commodi, aliquam tempore incidunt quos doloremque. Ipsum veniam doloremque dolor harum at
+        deleniti soluta quibusdam earum doloribus, quo accusantium delectus. Fuga perferendis natus aliquid voluptatibus
+        molestias. Quisquam deserunt, atque ratione aperiam veritatis cumque.</p>
+@endsection
